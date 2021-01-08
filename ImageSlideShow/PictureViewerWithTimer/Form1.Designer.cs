cd66673d4a5218b72fe535c56db65d7c2fa4c507
NadirLitgrey <75886853+NadirLitgrey@users.noreply.github.com>
2021-01-08 16:41:31 +0500

    Add files via upload

---
+++ ImageSlideShow/PictureViewerWithTimer/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PictureViewerWithTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.previous = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
+            this.play = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // previous
+            // 
+            this.previous.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previous.Location = new System.Drawing.Point(89, 46);
+            this.previous.Name = "previous";
+            this.previous.Size = new System.Drawing.Size(108, 29);
+            this.previous.TabIndex = 0;
+            this.previous.Text = "Previous";
+            this.previous.UseVisualStyleBackColor = true;
+            this.previous.Click += new System.EventHandler(this.previous_Click);
+            // 
+            // next
+            // 
+            this.next.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next.Location = new System.Drawing.Point(281, 46);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(108, 29);
+            this.next.TabIndex = 1;
+            this.next.Text = "Next";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // close
+            // 
+            this.close.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.Location = new System.Drawing.Point(471, 46);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(108, 29);
+            this.close.TabIndex = 2;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // play
+            // 
+            this.play.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.play.Location = new System.Drawing.Point(203, 427);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(108, 29);
+            this.play.TabIndex = 3;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // pause
+            // 
+            this.pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pause.Location = new System.Drawing.Point(352, 427);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(108, 29);
+            this.pause.TabIndex = 4;
+            this.pause.Text = "Pause";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(89, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(490, 302);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 483);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.previous);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button previous;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ImageSlideShow/PictureViewerWithTimer/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewerWithTimer
+{
+    public partial class Form1 : Form
+    {
+        String[] images;
+        static int imageNumber = 0;
+        static Boolean previousButtonClicked = false;
+        static Boolean nextButtonClicked = false;
+        static Boolean flowIsDefault=true;
+        public Form1()
+        {
+            InitializeComponent();
+            images = new String[7];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            char ch = 'A';
+            for (int i = 0; i < 6; i++)
+            {
+                images[i] = @"Resources\" + ch + ".jpg";
+                ch++;
+            }
+            pictureBox1.Image = Image.FromFile(images[imageNumber]);
+
+        }
+
+
+        private void showNextImages()
+        {
+            if (imageNumber < images.Length - 2)
+            {
+                imageNumber++;
+                pictureBox1.Image = Image.FromFile(images[imageNumber]);
+                
+            }
+            else
+            {
+                imageNumber = 0;
+                pictureBox1.Image = Image.FromFile(images[imageNumber]);
+                
+            }
+        }
+
+        private void previous_Click(object sender, EventArgs e)
+        {
+            showPreviousImage();
+            previousButtonClicked = true;
+            nextButtonClicked = false;
+        }
+
+        private void showPreviousImage()
+        {
+            imageNumber--;
+            if (imageNumber >= 0)
+            {
+
+                pictureBox1.Image = Image.FromFile(images[imageNumber]);
+
+            }
+            else
+            {
+                imageNumber = 5;
+                pictureBox1.Image = Image.FromFile(images[imageNumber]);
+
+            }
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            showNextImages();
+            nextButtonClicked = true;
+            previousButtonClicked = false;
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            flowIsDefault = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (nextButtonClicked || flowIsDefault)
+            {
+                showNextImages();
+                
+            }
+            else{
+                showPreviousImage();
+            }
+
+        }
+    }
+}
