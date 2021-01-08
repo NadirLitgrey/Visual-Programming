5d034f15ab19884243bfd3af921f362f1d9ddb31
NadirLitgrey <75886853+NadirLitgrey@users.noreply.github.com>
2021-01-08 16:34:55 +0500

    Add files via upload

---
+++ ImagePuzzleGame/Task3_PuzzleGame/Task3_PuzzleGame/FormPuzzle.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Task3_PuzzleGame
+{
+    partial class FormPuzzle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoBtn = new System.Windows.Forms.Button();
+            this.picBtn = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn15 = new System.Windows.Forms.Button();
+            this.btn16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // infoBtn
+            // 
+            this.infoBtn.Enabled = false;
+            this.infoBtn.Location = new System.Drawing.Point(128, 4);
+            this.infoBtn.Name = "infoBtn";
+            this.infoBtn.Size = new System.Drawing.Size(346, 102);
+            this.infoBtn.TabIndex = 1;
+            this.infoBtn.Text = "Find This Image within 3 Clicks on Following Buttons";
+            this.infoBtn.UseVisualStyleBackColor = true;
+            this.infoBtn.Click += new System.EventHandler(this.infoBtn_Click);
+            // 
+            // picBtn
+            // 
+            this.picBtn.Location = new System.Drawing.Point(5, 4);
+            this.picBtn.Name = "picBtn";
+            this.picBtn.Size = new System.Drawing.Size(104, 102);
+            this.picBtn.TabIndex = 0;
+            this.picBtn.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(5, 117);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(103, 95);
+            this.btn1.TabIndex = 2;
+            this.btn1.Tag = "SubPics";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(128, 117);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(103, 95);
+            this.btn2.TabIndex = 3;
+            this.btn2.Tag = "SubPics";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(250, 117);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(103, 95);
+            this.btn3.TabIndex = 4;
+            this.btn3.Tag = "SubPics";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(371, 117);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(103, 95);
+            this.btn4.TabIndex = 5;
+            this.btn4.Tag = "SubPics";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(371, 221);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(103, 95);
+            this.btn5.TabIndex = 9;
+            this.btn5.Tag = "SubPics";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(250, 221);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(103, 95);
+            this.btn6.TabIndex = 8;
+            this.btn6.Tag = "SubPics";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(128, 221);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(103, 95);
+            this.btn7.TabIndex = 7;
+            this.btn7.Tag = "SubPics";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(6, 221);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(103, 95);
+            this.btn8.TabIndex = 6;
+            this.btn8.Tag = "SubPics";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(371, 324);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(103, 95);
+            this.btn9.TabIndex = 13;
+            this.btn9.Tag = "SubPics";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(250, 324);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(103, 95);
+            this.btn10.TabIndex = 12;
+            this.btn10.Tag = "SubPics";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(125, 324);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(103, 95);
+            this.btn11.TabIndex = 11;
+            this.btn11.Tag = "SubPics";
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.btn11_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(6, 324);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(103, 95);
+            this.btn12.TabIndex = 10;
+            this.btn12.Tag = "SubPics";
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.Location = new System.Drawing.Point(371, 429);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(103, 95);
+            this.btn13.TabIndex = 17;
+            this.btn13.Tag = "SubPics";
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.btn13_Click);
+            // 
+            // btn14
+            // 
+            this.btn14.Location = new System.Drawing.Point(250, 429);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(103, 95);
+            this.btn14.TabIndex = 16;
+            this.btn14.Tag = "SubPics";
+            this.btn14.UseVisualStyleBackColor = true;
+            this.btn14.Click += new System.EventHandler(this.btn14_Click);
+            // 
+            // btn15
+            // 
+            this.btn15.Location = new System.Drawing.Point(128, 429);
+            this.btn15.Name = "btn15";
+            this.btn15.Size = new System.Drawing.Size(103, 95);
+            this.btn15.TabIndex = 15;
+            this.btn15.Tag = "SubPics";
+            this.btn15.UseVisualStyleBackColor = true;
+            this.btn15.Click += new System.EventHandler(this.btn15_Click);
+            // 
+            // btn16
+            // 
+            this.btn16.Location = new System.Drawing.Point(6, 429);
+            this.btn16.Name = "btn16";
+            this.btn16.Size = new System.Drawing.Size(103, 95);
+            this.btn16.TabIndex = 14;
+            this.btn16.Tag = "SubPics";
+            this.btn16.UseVisualStyleBackColor = true;
+            this.btn16.Click += new System.EventHandler(this.btn16_Click);
+            // 
+            // FormPuzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 536);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn14);
+            this.Controls.Add(this.btn15);
+            this.Controls.Add(this.btn16);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.infoBtn);
+            this.Controls.Add(this.picBtn);
+            this.Name = "FormPuzzle";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormPuzzle_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button infoBtn;
+        private System.Windows.Forms.Button picBtn;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn15;
+        private System.Windows.Forms.Button btn16;
+    }
+}
+
---
+++ ImagePuzzleGame/Task3_PuzzleGame/Task3_PuzzleGame/FormPuzzle.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Task3_PuzzleGame
+{
+    public partial class FormPuzzle : Form
+    {
+
+        String[] images= new String[16];
+        static String originalPicPath = "";
+        static int chances = 1;
+        static Button[] buttonArray= new Button[16];
+        public FormPuzzle()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void FormPuzzle_Load(object sender, EventArgs e)
+        {
+            LoadImages();
+        }
+        private void infoBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void LoadImages()
+        {
+
+            /*
+             Place random pic at original place
+             */
+            
+            Random random = new Random();
+            int randomNumber1 = random.Next(1, 17);
+            
+            originalPicPath = @"F:\5th Semester\Visual Programming\Labs\Lab_04\Task3_PuzzleGame\Task3_PuzzleGame\puzzle-pictures\candy" + randomNumber1 + ".jpg";
+            this.picBtn.Image = Image.FromFile(originalPicPath);
+
+            LoadButtons();
+
+            //Initialize String array objects
+
+            for (int i = 0; i < 16; i++) {
+                images[i] = "";
+            }
+            
+            SetOriginalPicAtRandomPositions();
+            SetRestOfPics();
+
+        }
+        public void LoadButtons() {
+            
+            int i = 15;            
+            foreach (Control c in this.Controls) {
+
+                if (c is Button && c.Tag== "SubPics") {
+                    buttonArray[i] = (Button)c;
+                    buttonArray[i].Text = ""+i;
+
+                    i--;
+                }
+            }
+        }
+        public void SetOriginalPicAtRandomPositions() {
+            
+            Random random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                int randomNumber2 = random.Next(0, 15);
+                if (images[randomNumber2].Length == 0)
+                {
+                    images[randomNumber2] = originalPicPath;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+        }
+        public void SetRestOfPics() {
+
+            
+            Random random = new Random();
+            for (int i = 0; i < 13; i++)
+            {
+                int randomNumber1 = random.Next(1, 17);
+                int randomNumber2 = random.Next(0, 16);
+                String path= @"F:\5th Semester\Visual Programming\Labs\Lab_04\Task3_PuzzleGame\Task3_PuzzleGame\puzzle-pictures\candy" + randomNumber1 + ".jpg";
+                if (images[randomNumber2].Length == 0 && path!=originalPicPath)
+                {
+                    images[randomNumber2] = path;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+        }
+        public void SetOnButtons(int index) {
+
+                buttonArray[index].Image =Image.FromFile(images[index]);
+            if (chances < 3)
+            {
+                if (images[index] == originalPicPath)
+                {
+                    DialogResult result = MessageBox.Show("You Won The Game", "Game Over", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+
+                        ReplayGame();
+                    }
+                    else
+                    {
+                        Application.Exit();
+                    }
+                }
+            }
+            else {
+                DialogResult result = MessageBox.Show("Do you want to play again?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    ReplayGame();
+                }
+                else {
+                    Application.Exit();
+                }
+            }
+            chances++;
+        }
+        public void ReplayGame() {
+
+
+            foreach (Button b in buttonArray) {
+
+                b.Text = "";
+                b.Image = null;
+            }
+            this.picBtn.Image = null;
+            
+
+            this.Load += FormPuzzle_Load;
+            chances = 0;
+            originalPicPath = "";
+
+            Random random = new Random();
+            int randomNumber1 = random.Next(1, 17);
+            originalPicPath = @"F:\5th Semester\Visual Programming\Labs\Lab_04\Task3_PuzzleGame\Task3_PuzzleGame\puzzle-pictures\candy" + randomNumber1 + ".jpg";
+            this.picBtn.Image = Image.FromFile(originalPicPath);
+
+        }
+
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(0);   
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+
+            SetOnButtons(1);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+            SetOnButtons(2);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+
+            SetOnButtons(3);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(7);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(5);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(4);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(11);
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(10);
+        }
+
+        private void btn11_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(9);
+        }
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(8);
+        }
+
+        private void btn13_Click(object sender, EventArgs e)
+        {
+
+            SetOnButtons(15);
+        }
+
+        private void btn14_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(14);
+        }
+
+        private void btn15_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(13);
+        }
+
+        private void btn16_Click(object sender, EventArgs e)
+        {
+            SetOnButtons(12);
+        }
+
+    }
+}
