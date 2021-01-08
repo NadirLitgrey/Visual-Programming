1e8340c06a1af808c355db8d21507dbd14f21b9a
NadirLitgrey <75886853+NadirLitgrey@users.noreply.github.com>
2021-01-08 16:47:07 +0500

    Add files via upload

---
+++ Image Slide Show/SlideShowApp/SlideShowApp/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace SlideShowApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.drivesBox = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.previousBtn = new System.Windows.Forms.Button();
+            this.playBtn = new System.Windows.Forms.Button();
+            this.pauseBtn = new System.Windows.Forms.Button();
+            this.firstBtn = new System.Windows.Forms.Button();
+            this.lastBtn = new System.Windows.Forms.Button();
+            this.directoryBox = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // drivesBox
+            // 
+            this.drivesBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.drivesBox.FormattingEnabled = true;
+            this.drivesBox.ItemHeight = 13;
+            this.drivesBox.Location = new System.Drawing.Point(12, 22);
+            this.drivesBox.Name = "drivesBox";
+            this.drivesBox.Size = new System.Drawing.Size(189, 21);
+            this.drivesBox.TabIndex = 0;
+            this.drivesBox.Text = "Select Drive";
+            this.drivesBox.SelectedIndexChanged += new System.EventHandler(this.drivesBox_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(210, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(394, 310);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nextBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nextBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextBtn.Location = new System.Drawing.Point(20, 27);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(85, 31);
+            this.nextBtn.TabIndex = 2;
+            this.nextBtn.Text = "Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // previousBtn
+            // 
+            this.previousBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.previousBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previousBtn.Location = new System.Drawing.Point(20, 80);
+            this.previousBtn.Name = "previousBtn";
+            this.previousBtn.Size = new System.Drawing.Size(85, 31);
+            this.previousBtn.TabIndex = 3;
+            this.previousBtn.Text = "Previous";
+            this.previousBtn.UseVisualStyleBackColor = true;
+            this.previousBtn.Click += new System.EventHandler(this.previousBtn_Click);
+            // 
+            // playBtn
+            // 
+            this.playBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.playBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playBtn.Location = new System.Drawing.Point(20, 133);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(85, 31);
+            this.playBtn.TabIndex = 4;
+            this.playBtn.Text = "Play";
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
+            // 
+            // pauseBtn
+            // 
+            this.pauseBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pauseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pauseBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseBtn.Location = new System.Drawing.Point(20, 181);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(85, 31);
+            this.pauseBtn.TabIndex = 5;
+            this.pauseBtn.Text = "Pause";
+            this.pauseBtn.UseVisualStyleBackColor = true;
+            this.pauseBtn.Click += new System.EventHandler(this.pauseBtn_Click);
+            // 
+            // firstBtn
+            // 
+            this.firstBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.firstBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.firstBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstBtn.Location = new System.Drawing.Point(20, 237);
+            this.firstBtn.Name = "firstBtn";
+            this.firstBtn.Size = new System.Drawing.Size(85, 31);
+            this.firstBtn.TabIndex = 6;
+            this.firstBtn.Text = "First";
+            this.firstBtn.UseVisualStyleBackColor = true;
+            this.firstBtn.Click += new System.EventHandler(this.firstBtn_Click);
+            // 
+            // lastBtn
+            // 
+            this.lastBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lastBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lastBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastBtn.Location = new System.Drawing.Point(20, 291);
+            this.lastBtn.Name = "lastBtn";
+            this.lastBtn.Size = new System.Drawing.Size(85, 31);
+            this.lastBtn.TabIndex = 7;
+            this.lastBtn.Text = "Last";
+            this.lastBtn.UseVisualStyleBackColor = true;
+            this.lastBtn.Click += new System.EventHandler(this.lastBtn_Click);
+            // 
+            // directoryBox
+            // 
+            this.directoryBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.directoryBox.FormattingEnabled = true;
+            this.directoryBox.Location = new System.Drawing.Point(12, 56);
+            this.directoryBox.Name = "directoryBox";
+            this.directoryBox.Size = new System.Drawing.Size(189, 277);
+            this.directoryBox.Sorted = true;
+            this.directoryBox.TabIndex = 8;
+            this.directoryBox.SelectedIndexChanged += new System.EventHandler(this.directoryBox_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.pauseBtn);
+            this.panel1.Controls.Add(this.nextBtn);
+            this.panel1.Controls.Add(this.lastBtn);
+            this.panel1.Controls.Add(this.previousBtn);
+            this.panel1.Controls.Add(this.firstBtn);
+            this.panel1.Controls.Add(this.playBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(624, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(117, 378);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(741, 378);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.directoryBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.drivesBox);
+            this.MaximumSize = new System.Drawing.Size(757, 417);
+            this.MinimumSize = new System.Drawing.Size(757, 417);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Advanced Image Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox drivesBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button previousBtn;
+        private System.Windows.Forms.Button playBtn;
+        private System.Windows.Forms.Button pauseBtn;
+        private System.Windows.Forms.Button firstBtn;
+        private System.Windows.Forms.Button lastBtn;
+        private System.Windows.Forms.ListBox directoryBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Image Slide Show/SlideShowApp/SlideShowApp/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace SlideShowApp
+{
+    public partial class Form1 : Form
+    {
+        static String[] imagePath=new string[100];
+        static int imageIndex = 0;
+        static int index = 0;
+
+
+        static Boolean previousButtonClicked = false;
+        static Boolean nextButtonClicked = false;
+        static Boolean flowIsDefault = true;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in drives) {
+                drivesBox.Items.Add(d.Name);
+            }    
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            showNextImages();
+
+            nextButtonClicked = true;
+            previousButtonClicked = false;
+
+        }
+
+        private void showNextImages()
+        {
+            if (imageIndex < index-1)
+            {
+                imageIndex++;
+                pictureBox1.Image = Image.FromFile(imagePath[imageIndex]);
+
+            }
+            else
+            {
+                imageIndex = 0;
+                pictureBox1.Image = Image.FromFile(imagePath[imageIndex]);
+
+            }
+        }
+
+        private void previousBtn_Click(object sender, EventArgs e)
+        {
+            showPreviousImages();
+
+            previousButtonClicked = true;
+            nextButtonClicked = false;
+        }
+
+        private void showPreviousImages()
+        {
+            imageIndex--;
+            if (imageIndex >= 0)
+            {
+                pictureBox1.Image = Image.FromFile(imagePath[imageIndex]);
+
+            }
+            else
+            {
+                imageIndex = index-1;
+                pictureBox1.Image = Image.FromFile(imagePath[imageIndex]);
+
+            }
+        }
+
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void pauseBtn_Click(object sender, EventArgs e)
+        {
+
+            timer1.Stop();
+            flowIsDefault = false;
+        }
+
+        private void firstBtn_Click(object sender, EventArgs e)
+        {
+            imageIndex = 0;
+            pictureBox1.Image = Image.FromFile(imagePath[imageIndex]);
+        }
+
+        private void lastBtn_Click(object sender, EventArgs e)
+        {
+            imageIndex = index-1;
+            pictureBox1.Image = Image.FromFile(imagePath[imageIndex]);
+        }
+
+        private void drivesBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            imagePath = new string[100];
+            imageIndex = 0;
+            index = 0;
+
+
+            previousButtonClicked = false;
+            nextButtonClicked = false;
+            flowIsDefault = true;
+
+            DirectoryInfo dir = new DirectoryInfo(drivesBox.Text);
+            DirectoryInfo[] directories = dir.GetDirectories();
+            directoryBox.Items.Clear();
+            foreach (DirectoryInfo d in directories) {
+                directoryBox.Items.Add(d.Name);
+            }
+        }
+
+        private void directoryBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String str = directoryBox.SelectedItem.ToString();
+            
+            FileInfo i = new FileInfo(drivesBox.SelectedItem.ToString()+str+@"\");
+            FileInfo[] files = i.Directory.GetFiles();
+            int counter = 0;
+            
+            foreach (FileInfo fi in files)
+            {
+                if (fi.Name.ToUpper().EndsWith("PNG") || fi.Name.ToUpper().EndsWith("JPG"))
+                {
+                    counter++;
+                }
+            }
+            
+            foreach (FileInfo fi in files)
+            {
+                if (fi.Name.ToUpper().EndsWith("PNG") || fi.Name.ToUpper().EndsWith("JPG"))
+                {
+                    imagePath[index]= fi.FullName;
+                    index++;
+                }
+            }
+            if (index > 0) {
+                pictureBox1.Image = Image.FromFile(imagePath[imageIndex]);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (nextButtonClicked || flowIsDefault)
+            {
+                showNextImages();
+
+            }
+            else
+            {
+                showPreviousImages();
+            }
+        }
+    }
+}
