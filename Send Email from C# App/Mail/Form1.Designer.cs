2b251d637d84c8f8cebe79260ab632ea11d238a4
NadirLitgrey <75886853+NadirLitgrey@users.noreply.github.com>
2021-01-08 17:24:24 +0500

    Add files via upload

---
+++ Send Email from C# App/Mail/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Mail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtCC = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAttach = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(365, 399);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(213, 23);
+            this.btnSend.TabIndex = 35;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(113, 68);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(465, 20);
+            this.txtPass.TabIndex = 29;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(113, 39);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(465, 20);
+            this.txtUserName.TabIndex = 28;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Sender Email";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(113, 211);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(465, 152);
+            this.txtMessage.TabIndex = 25;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(113, 185);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(465, 20);
+            this.txtSubject.TabIndex = 24;
+            // 
+            // txtCC
+            // 
+            this.txtCC.Location = new System.Drawing.Point(113, 154);
+            this.txtCC.Name = "txtCC";
+            this.txtCC.Size = new System.Drawing.Size(465, 20);
+            this.txtCC.TabIndex = 23;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(113, 128);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(465, 20);
+            this.txtTo.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Message:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Subject:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "CC:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Receiver email";
+            // 
+            // btnAttach
+            // 
+            this.btnAttach.Location = new System.Drawing.Point(113, 399);
+            this.btnAttach.Name = "btnAttach";
+            this.btnAttach.Size = new System.Drawing.Size(232, 23);
+            this.btnAttach.TabIndex = 36;
+            this.btnAttach.Text = "Attach";
+            this.btnAttach.UseVisualStyleBackColor = true;
+            this.btnAttach.Click += new System.EventHandler(this.btnAttach_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 450);
+            this.Controls.Add(this.btnAttach);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtCC);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtCC;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAttach;
+    }
+}
+
---
+++ Send Email from C# App/Mail/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace Mail
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+            NetworkCredential login= new NetworkCredential(txtUserName.Text, txtPass.Text);
+            SmtpClient client =client = new SmtpClient("smtp.gmail.com");
+            ;
+            MailMessage msg;
+
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.Credentials = login;
+            
+            msg = new MailMessage { From = new MailAddress(txtUserName.Text, "Admin", Encoding.UTF8) };
+            msg.To.Add(new MailAddress(txtTo.Text));
+            
+            if (!string.IsNullOrEmpty(txtCC.Text))
+                msg.To.Add(new MailAddress(txtCC.Text));
+            
+            msg.Subject = txtSubject.Text;
+            
+            msg.Body = txtMessage.Text;
+            if (filePath.Length > 0)
+            {
+                msg.Attachments.Add(new Attachment(filePath));
+            }
+            msg.BodyEncoding = Encoding.UTF8;
+            
+            msg.IsBodyHtml = true;
+            
+            msg.Priority = MailPriority.Normal;
+            msg.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            client.SendCompleted += new SendCompletedEventHandler(SendCompletedCallBack);
+            string userstate = "Sending....";
+            client.SendAsync(msg, userstate);
+        }
+        private static void SendCompletedCallBack(object sender, AsyncCompletedEventArgs e)
+        {
+
+            if (e.Cancelled)
+                MessageBox.Show(string.Format("{0} send canceled.", e.UserState), "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (e.Error != null)
+                MessageBox.Show(string.Format("{0} {1}", e.UserState, e.Error), "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Mail sent Successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void btnAttach_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            if (opd.ShowDialog() == DialogResult.OK) {
+                string path = opd.FileName.ToString();
+                filePath = path;
+            }
+
+        }
+    }
+}
