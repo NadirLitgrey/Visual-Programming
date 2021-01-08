1e4cc1b8c2e3cc072df44cc4e6fd70bc977efe5d
NadirLitgrey <75886853+NadirLitgrey@users.noreply.github.com>
2021-01-08 16:15:11 +0500

    Add files via upload

---
+++ Tic-Tac-Toe Simple/Tik_Tac_Toe/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Tik_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.BtnSix = new System.Windows.Forms.Button();
+            this.BtnFive = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnAgain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOne
+            // 
+            this.btnOne.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOne.Location = new System.Drawing.Point(8, 76);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(110, 86);
+            this.btnOne.TabIndex = 0;
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwo.Location = new System.Drawing.Point(124, 76);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(110, 86);
+            this.btnTwo.TabIndex = 1;
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThree.Location = new System.Drawing.Point(240, 76);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(110, 86);
+            this.btnThree.TabIndex = 2;
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // BtnSix
+            // 
+            this.BtnSix.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSix.Location = new System.Drawing.Point(240, 174);
+            this.BtnSix.Name = "BtnSix";
+            this.BtnSix.Size = new System.Drawing.Size(110, 86);
+            this.BtnSix.TabIndex = 5;
+            this.BtnSix.UseVisualStyleBackColor = true;
+            this.BtnSix.Click += new System.EventHandler(this.BtnSix_Click);
+            // 
+            // BtnFive
+            // 
+            this.BtnFive.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFive.Location = new System.Drawing.Point(124, 174);
+            this.BtnFive.Name = "BtnFive";
+            this.BtnFive.Size = new System.Drawing.Size(110, 86);
+            this.BtnFive.TabIndex = 4;
+            this.BtnFive.UseVisualStyleBackColor = true;
+            this.BtnFive.Click += new System.EventHandler(this.BtnFive_Click);
+            // 
+            // btnFour
+            // 
+            this.btnFour.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFour.Location = new System.Drawing.Point(8, 174);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(110, 86);
+            this.btnFour.TabIndex = 3;
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // btnNine
+            // 
+            this.btnNine.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNine.Location = new System.Drawing.Point(240, 266);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(110, 86);
+            this.btnNine.TabIndex = 8;
+            this.btnNine.UseVisualStyleBackColor = true;
+            this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+            // 
+            // btnEight
+            // 
+            this.btnEight.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEight.Location = new System.Drawing.Point(124, 266);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(110, 86);
+            this.btnEight.TabIndex = 7;
+            this.btnEight.UseVisualStyleBackColor = true;
+            this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeven.Location = new System.Drawing.Point(8, 266);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(110, 86);
+            this.btnSeven.TabIndex = 6;
+            this.btnSeven.UseVisualStyleBackColor = true;
+            this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(21, 35);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(114, 18);
+            this.lblInfo.TabIndex = 9;
+            this.lblInfo.Text = "Winner Info: ";
+            // 
+            // btnAgain
+            // 
+            this.btnAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgain.Location = new System.Drawing.Point(203, 25);
+            this.btnAgain.Name = "btnAgain";
+            this.btnAgain.Size = new System.Drawing.Size(147, 34);
+            this.btnAgain.TabIndex = 10;
+            this.btnAgain.Text = "Play Again";
+            this.btnAgain.UseVisualStyleBackColor = true;
+            this.btnAgain.Click += new System.EventHandler(this.btnAgain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 368);
+            this.Controls.Add(this.btnAgain);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.BtnSix);
+            this.Controls.Add(this.BtnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button BtnSix;
+        private System.Windows.Forms.Button BtnFive;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnAgain;
+    }
+}
+
---
+++ Tic-Tac-Toe Simple/Tik_Tac_Toe/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tik_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public TicTacToe obj = new TicTacToe();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int btnNumber=0;
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            if (obj.getTogglevalue())
+            {
+
+                btnOne.Text = obj.getUserOne();
+                obj.setToggleValue(false);
+            }
+            else
+            {
+                btnOne.Text = obj.getUserTwo();
+                obj.setToggleValue(true);
+            }
+            btnOne.Enabled = false;
+            checkForWinner();
+            btnNumber++;
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+            if (obj.getTogglevalue())
+            {
+
+                btnTwo.Text = obj.getUserOne();
+                obj.setToggleValue(false);
+            }
+            else
+            {
+                btnTwo.Text = obj.getUserTwo();
+                obj.setToggleValue(true);
+            }
+            btnTwo.Enabled = false;
+            checkForWinner();
+            btnNumber++;
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            if (obj.getTogglevalue())
+            {
+
+                btnThree.Text = obj.getUserOne();
+                obj.setToggleValue(false);
+            }
+            else
+            {
+                btnThree.Text = obj.getUserTwo();
+                obj.setToggleValue(true);
+            }
+            btnThree.Enabled = false;
+            checkForWinner();
+            btnNumber++;
+        }
+
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+
+            if (obj.getTogglevalue())
+            {
+
+                btnFour.Text = obj.getUserOne();
+                obj.setToggleValue(false);
+            }
+            else
+            {
+                btnFour.Text = obj.getUserTwo();
+                obj.setToggleValue(true);
+            }
+            btnFour.Enabled = false;
+            checkForWinner();
+            btnNumber++;
+        }
+
+        private void BtnFive_Click(object sender, EventArgs e)
+        {
+
+            if (obj.getTogglevalue())
+            {
+
+
+                BtnFive.Text = obj.getUserOne();
+                obj.setToggleValue(false);
+            }
+            else
+            {
+                BtnFive.Text = obj.getUserTwo();
+                obj.setToggleValue(true);
+            }
+            BtnFive.Enabled = false;
+            checkForWinner();
+            btnNumber++;
+        }
+
+        private void BtnSix_Click(object sender, EventArgs e)
+        {
+
+            if (obj.getTogglevalue())
+            {
+
+                BtnSix.Text = obj.getUserOne();
+                obj.setToggleValue(false);
+            }
+            else
+            {
+                BtnSix.Text = obj.getUserTwo();
+                obj.setToggleValue(true);
+            }
+            BtnSix.Enabled = false;
+            checkForWinner();
+            btnNumber++;
+        }
+
+        private void btnSeven_Click(object sender, EventArgs e)
+        {
+
+            if (obj.getTogglevalue())
+            {
+
+                btnSeven.Text = obj.getUserOne();
+                obj.setToggleValue(false);
+            }
+            else
+            {
+                btnSeven.Text = obj.getUserTwo();
+                obj.setToggleValue(true);
+            }
+            btnSeven.Enabled = false;
+            checkForWinner();
+            btnNumber++;
+        }
+
+        private void btnEight_Click(object sender, EventArgs e)
+        {
+
+            if (obj.getTogglevalue())
+            {
+
+                btnEight.Text = obj.getUserOne();
+                obj.setToggleValue(false);
+            }
+            else
+            {
+                btnEight.Text = obj.getUserTwo();
+                obj.setToggleValue(true);
+            }
+            btnEight.Enabled = false;
+            checkForWinner();
+            btnNumber++;
+        }
+
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+
+            if (obj.getTogglevalue())
+            {
+
+                btnNine.Text = obj.getUserOne();
+                obj.setToggleValue(false);
+            }
+            else
+            {
+                btnNine.Text = obj.getUserTwo();
+                obj.setToggleValue(true);
+            }
+            btnNine.Enabled = false;
+            checkForWinner();
+            btnNumber++;
+        }
+        public void checkForWinner()
+        {
+            String[,] box ={
+                                                {btnOne.Text,btnTwo.Text, btnThree.Text},
+                                                {btnFour.Text, BtnFive.Text, BtnSix.Text},
+                                                {btnSeven.Text, btnEight.Text, btnNine.Text}
+                                            };
+
+            Boolean won_X = false, won_O = false; ;
+            for (int row = 0; row < 3; row++) {
+
+                for (int column = 0; column < 3; column++) {
+                    if (box[row, column] == "X")
+                    {
+                        won_X = checkForX(box, row, column);
+                        
+                    }
+                    else if(box[row,column]=="O"){
+
+                        won_O = checkColumnWiseFor_O(box, row, column);
+                    }
+                }
+            }
+            if (won_X)
+            {
+                MessageBox.Show("Hurrah X Won!\nCongratulations To the winner");
+                lblInfo.Text = "X Won";
+                DisableAll();
+            }
+            if (won_O)
+            {
+                MessageBox.Show("Hurrah O Won!\nCongratulations To the winner");
+                lblInfo.Text = "O Won";
+                DisableAll();
+            }
+            else if(btnNumber>=8){
+                    MessageBox.Show("Very Close Contest.. Try again!");
+                    lblInfo.Text = "Match Tied";
+                    DisableAll();
+            } 
+        }
+        public static Boolean checkForX(String[,] box,int row, int col){
+
+            int count1 = 0, count2 = 0, count3=0, count4=0;
+            int k = 0, l=2;
+            for (int j = 0; j < 3; j++)
+            {
+                if (box[row, j] == "X")
+                {
+                    count1++;
+                }
+                if (box[j, col] == "X")
+                {
+                    count2++;
+                }
+                if (k==j && box[j, k] == "X") {
+                    k++;
+                    count3++;
+                }
+                if (l+j==2 && box[j, l] == "X")
+                {
+                    l -= 1;
+                    count4++;
+                }
+
+            }
+            if (count1 == 3 || count2==3 || count3==3 || count4==3) {
+                return true;
+            }
+            return false;
+        }
+        public static Boolean checkColumnWiseFor_O(String[,] box, int row, int col)
+        {
+
+            int count1 = 0, count2 = 0, count3=0, count4=0, k=0, l=2;
+
+            for (int j = 0; j < 3; j++)
+            {
+                if (box[row, j] == "O")
+                {
+                    count1++;
+                }
+                if (box[j, col] == "O")
+                {
+                    count2++;
+                }
+                if (k == j && box[j, k] == "O")
+                {
+                    k++;
+                    count3++;
+                }
+                if (l + j == 2 && box[j, l] == "O")
+                {
+                    l -= 1;
+                    count4++;
+                }
+
+            }
+            if (count1 == 3 || count2 == 3 || count3 == 3 || count4 == 3)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void DisableAll() {
+
+            btnNine.Enabled = false;
+            btnEight.Enabled = false;
+            btnSeven.Enabled = false;
+            BtnSix.Enabled = false;
+            BtnFive.Enabled = false;
+            btnFour.Enabled = false;
+            btnThree.Enabled = false;
+            btnTwo.Enabled = false;
+            btnOne.Enabled = false;
+            
+        }
+        public void EnableAll() {
+
+
+            btnNine.Text = "";
+            btnEight.Text = "";
+            btnSeven.Text = "";
+            BtnSix.Text = "";
+            BtnFive.Text = "";
+            btnFour.Text = "";
+            btnThree.Text = "";
+            btnTwo.Text = "";
+            btnOne.Text = "";
+
+            btnNine.Enabled = true;
+            btnEight.Enabled = true;
+            btnSeven.Enabled = true;
+            BtnSix.Enabled = true;
+            BtnFive.Enabled = true;
+            btnFour.Enabled = true;
+            btnThree.Enabled = true;
+            btnTwo.Enabled = true;
+            btnOne.Enabled = true;
+
+            btnNumber = 0;
+        }
+        private void btnAgain_Click(object sender, EventArgs e)
+        {
+            EnableAll();
+        }
+    }
+}
