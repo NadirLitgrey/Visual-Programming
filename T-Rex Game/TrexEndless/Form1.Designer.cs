7b3bc02d3eb831f756dd1eeddf0fbfa8ce5d3085
NadirLitgrey <75886853+NadirLitgrey@users.noreply.github.com>
2021-01-08 16:44:15 +0500

    Add files via upload

---
+++ T-Rex Game/TrexEndless/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TrexEndless
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.score = new System.Windows.Forms.Label();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bird
+            // 
+            this.bird.Image = global::TrexEndless.Properties.Resources.running;
+            this.bird.Location = new System.Drawing.Point(98, 389);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(40, 43);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.Image = global::TrexEndless.Properties.Resources.obstacle_1;
+            this.obstacle1.Location = new System.Drawing.Point(403, 389);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(23, 46);
+            this.obstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.obstacle1.TabIndex = 1;
+            this.obstacle1.TabStop = false;
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.Image = global::TrexEndless.Properties.Resources.obstacle_2;
+            this.obstacle2.Location = new System.Drawing.Point(565, 399);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(32, 33);
+            this.obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.obstacle2.TabIndex = 2;
+            this.obstacle2.TabStop = false;
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(69, 43);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(67, 23);
+            this.score.TabIndex = 1;
+            this.score.Text = "Score:";
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ground.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ground.Location = new System.Drawing.Point(0, 430);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(800, 20);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ground.TabIndex = 3;
+            this.ground.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.obstacle2);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.bird);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ T-Rex Game/TrexEndless/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TrexEndless
+{
+    public partial class Form1 : Form
+    {
+        int obstacle_move_left_distance = 10;
+        int bird_jump=0;
+        static int score_count = 0, count_jumps=0;
+        int bird_distance_from_left = 110, interval=0;
+        static Boolean  completed = false, grounded=true;
+        static int speed = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+         //   timer2.Start();
+
+        }
+        private void moveObstacles() {
+            
+            obstacle1.Left -= obstacle_move_left_distance+10;
+            obstacle2.Left -= obstacle_move_left_distance+10;
+            Random rand = new Random();
+            
+            if (obstacle1.Left <= (0 - obstacle1.Width)) {
+                int number = rand.Next(bird_distance_from_left + 200, this.Width);
+                obstacle1.Left = number;
+                score_count++;
+            }
+            if (obstacle2.Left <= (0 - obstacle2.Width))
+            {
+                int number = rand.Next(bird_distance_from_left + 200, this.Width);
+                obstacle2.Left = number;
+                score_count++;
+            }
+            
+            if (obstacle1.Bounds.IntersectsWith(obstacle2.Bounds)) {
+                obstacle2.Left += obstacle1.Width*4;
+            }
+            
+            if (bird.Bounds.IntersectsWith(obstacle1.Bounds) || bird.Bounds.IntersectsWith(obstacle2.Bounds))
+            {
+                bird.Image = TrexEndless.Properties.Resources.dead;
+                timer1.Stop();
+                MessageBox.Show("Game Over (:");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            score.Text = "Score: " + score_count;
+            moveObstacles();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && grounded==true) {
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (interval <= 6 && completed == false/* && bird.Top >= 250*/)
+            {
+                bird.Top -= 40;
+                interval++;
+                if (interval == 6)
+                {
+                    completed = true;
+                }
+                count_jumps++;
+                grounded = false;
+            }
+            if (interval > 0 && completed)
+            {
+
+                bird.Top += 40;
+                interval--;
+                if (interval == 0)
+                {
+                    completed = false;
+                }
+                count_jumps++;
+                grounded = false;
+            }
+            if (count_jumps >= 14)
+            {
+
+                timer2.Stop();
+                grounded = true;
+                count_jumps = 0;
+                bird.Top = 389;
+                interval = 0;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+                //timer1.Tick += Timer1_Tick1; ;
+            }
+            if (e.KeyCode == Keys.R) {
+                
+                score.Text = "Score: ";
+                obstacle1.Left = 600;
+                obstacle2.Left = 750;
+                score_count = 0;
+                bird.Image = TrexEndless.Properties.Resources.running;
+
+                grounded = true;
+                timer1.Start();
+            }
+        }
+        private void Timer1_Tick1(object sender, EventArgs e)
+        {
+            if (bird.Top <ground.Top)
+            {
+                bird.Top += 40;
+            }
+        }
+    }
+}
